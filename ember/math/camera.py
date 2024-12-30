approx correct viser unproject, zed meter units, next intrinsics

ember/math/camera.py
@@ -1,19 +1,62 @@
+"""
+Camera module for common imaging, and projective geometry operations.
+
+"Standard" Conventions:
+- World coordinates:
+    Origin: Center of the world, usually first camera position
+    Right: +X
+    Up: +Y
+    Forward: +Z
+- Camera coordinates:
+    Origin: Camera center
+    Right: +X
+    Up: -Y
+    Forward: +Z
+- Image coordinates:
+    Origin: Bottom left corner
+    Right: +X
+    Up: +Y
+    Forward: +Z
+"""
 import math
-from typing import Union
 import torch
 from torch import nn, Tensor
-import einops as eo
+from enum import Enum
 from jaxtyping import Float
+import viser
 
 from ember.math.homogenous import to_homogenous, from_homogenous
+from ember.math.quat import qvec2rotmat
+
+
+
+class CameraConvention(Enum):
+    """
+    Enum for camera coordinate conventions.
+
+    Expressed as Right, Up, Forward
+    """
+    STANDARD = "+X +Y +Z"
+    GSPLAT = dict(
+        world="+Z forward, +X right, +Y up",
+        camera="+Z forward, +X right, +Y down",
+        image="+Y down, +X right",
+    )
+    
+    VISER = COLMAP = OPENCV = "+X -Y +Z"
+    NERFSTUDIO = OPENGL = BLENDER = "+X +Y -Z"
 
 
 class Camera(nn.Module):
+    camera_to_world: Float[Tensor, "4 4"]
+    world_to_camera: Float[Tensor, "4 4"]
+    camera_to_image: Float[Tensor, "3 3"]
+    image_to_camera: Float[Tensor, "3 3"]
+
     def __init__(
         self,
         image_height: int,
         image_width: int,
-        fov_x: float = math.pi / 2.0,  # 90 degrees
     ):
         super().__init__()
 
@@ -22,29 +65,22 @@ class Camera(nn.Module):
         #   because the extrinsics transform moves the world origin
         #   to (0, 0, 1) in camera coordinates
         self.register_buffer(   
-            "extrinsics", torch.tensor(
-                [
-                    [1.0, 0.0, 0.0, 0.0],
-                    [0.0, 1.0, 0.0, 0.0],
-                    [0.0, 0.0, 1.0, 1.0],
-                    [0.0, 0.0, 0.0, 1.0],
-                ],
-                requires_grad=False,
-            )
+            "world_to_camera", torch.eye(4, requires_grad=False)
         )
         # Inverse extrinsics matrix: camera coordinates to world coordinates
         self.register_buffer(
-            "inverse_extrinsics", torch.inverse(self.extrinsics)
+            "camera_to_world", self.world_to_camera
         )
         
         # Intrinsics matrix: camera coordinates to image coordinates
         self.image_width = image_width
         self.image_height = image_height
+        fov_x = math.pi / 2.0  # 90 degrees
         self.focal = 0.5 * image_width / math.tan(0.5 * fov_x)
         # Focal length calculations https://www.desmos.com/calculator/plcm03jtw3
 
         self.register_buffer(
-            "intrinsics", torch.tensor(
+            "camera_to_image", torch.tensor(
                 [
                     [self.focal, 0, image_width / 2.],
                     [0, self.focal, image_height / 2.],
@@ -55,8 +91,32 @@ class Camera(nn.Module):
         )
         # Inverse intrinsics matrix: image coordinates to camera coordinates
         self.register_buffer(
-            "inverse_intrinsics", torch.inverse(self.intrinsics)
+            "image_to_camera", torch.inverse(self.camera_to_image)
         )
+
+    # @staticmethod
+    # def from_viser_camera(
+    #     viser_camera: viser.CameraHandle,
+    # ) -> "Camera":
+    #     """Create a Camera instance from a Viser camera handle"""
+    #     fov_y = viser_camera.fov
+    #     fov_x = viser_camera.aspect * fov_y
+    #     viser_camera.client.
+    #     viser_camera.wxyz
+    #     viser_camera.position
+
+    #     camera = Camera(
+    #         image_height=1,
+    #         image_width=viser_camera.width,
+    #     )
+    #     camera.extrinsics[:3, :3] = viser_camera.rotation
+
+    #     camera.extrinsics = viser_camera.extrinsics
+    #     camera.inverse_extrinsics = viser_camera.inverse_extrinsics
+    #     camera.intrinsics = viser_camera.intrinsics
+    #     camera.inverse_intrinsics = viser_camera.inverse_intrinsics
+
+    #     return camera
 
     def forward(
         self,
@@ -66,10 +126,10 @@ class Camera(nn.Module):
         points_world_h = to_homogenous(points_world)
         
         # Transform points from world coordinates to camera coordinates
-        points_camera_h = points_world_h @ self.extrinsics.T  # Left matrix-vector multiplication
+        points_camera_h = points_world_h @ self.world_to_camera.T  # Left matrix-vector multiplication
 
         # Transform points from camera coordinates to image coordinates
-        points_image_h = points_camera_h[..., :3] @ self.intrinsics.T  # Left matrix-vector multiplication
+        points_image_h = points_camera_h[..., :3] @ self.camera_to_image.T  # Left matrix-vector multiplication
 
         return from_homogenous(points_image_h)  # Return only x, y coordinate components
 
@@ -87,18 +147,20 @@ class Camera(nn.Module):
         # Convert pixel coordinates to camera coordinates
         #  Adding 0.5 to x_image and y_image to get the center of the pixel
         h = torch.ones_like(x_image)
-        xy_camera = torch.stack((x_image + 0.5, y_image + 0.5, h), dim=-1)
+        xyh_image = torch.stack((x_image + 0.5, y_image + 0.5, h), dim=-1).to(
+            dtype=depths.dtype, device=depths.device
+        )
         
-        xy_camera = xy_camera @ self.inverse_intrinsics.T
-        
-        points_camera = xy_camera * depths[..., None]
-        points_camera[..., -1] *= -1  # Camera looks along -z axis
+        xyh_camera = xyh_image @ self.image_to_camera.T
+
+        points_camera = xyh_camera * depths[..., None]
+        # points_camera[..., -1] *= -1  # Camera looks along -z axis
 
         # Create homogeneous coordinates
         points_camera_h = to_homogenous(points_camera)
 
         # Transform pixel coordinates to world coordinates
-        points_world_h = points_camera_h @ self.inverse_extrinsics.T
+        points_world_h = points_camera_h @ self.camera_to_world.T
         
         return from_homogenous(points_world_h)
 

ember/math/quat.py
@@ -1,6 +1,11 @@
 import torch
+from torch import Tensor
+from jaxtyping import Float
 
-def qvec2rotmat(qvec):
+
+def qvec2rotmat(
+    qvec: Float[Tensor, "4"],
+) -> Float[Tensor, "3 3"]:
     return torch.tensor(
         [
             [
@@ -20,5 +25,4 @@ def qvec2rotmat(qvec):
             ],
         ],
         dtype=torch.float32,
-        device="cuda",
     )